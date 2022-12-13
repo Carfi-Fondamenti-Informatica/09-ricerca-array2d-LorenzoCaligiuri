dc6946dc23eea422e72c8f0e38e92688f07bc75b
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-13 12:24:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,9 @@ int main() {
     char d1[20];
     char d2[10][20];
     int i=0;
-    cout<<"inserisci d2"<<endl;
     for(int j=0; j<10; j++){
         cin>>d2[j];
     }
-    cout<<"inserisci d1"<<endl;
     cin>>d1;
     if(f(d2, d1, i)==true){
         cout<<i;
