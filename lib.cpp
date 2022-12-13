60b33ec22eb368234e265bc1ff3e014c82632ae7
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-13 12:12:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+#include "lib.h"
+
+bool f(char d2[10][20], char d1[10],int &i){
+    for(int k=0; k<20; k++){
+        if(d1==d2[k]){
+            return true;
+        }else{
+            for(int j=0; j<10; j++){
+                if(d1[k]==d2[j][k]){
+                    return i;
+                }else{
+                    i++;
+                }
+            }
+            return false;
+        }
+    }
+}
 
