c3c5ebe5410dc2a3cc5e549523a02d7fe124b00b
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-13 12:10:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char d1[20];
+    char d2[10][20];
+    int i=0;
+    cout<<"inserisci d2"<<endl;
+    for(int j=0; j<10; j++){
+        cin>>d2[j];
+    }
+    cout<<"inserisci d1"<<endl;
+    cin>>d1;
+    if(f(d2, d1, i)==true){
+        cout<<i;
+    }else{
+        cout<<"non presente"<<endl;
+    }
+    return 0;
 }
+
