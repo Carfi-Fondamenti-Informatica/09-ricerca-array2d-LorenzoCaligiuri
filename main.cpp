6df65d0f97677015a87f862fcbfad9f141265ed3
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-15 14:01:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@ int main() {
     char d1[20];
     char d2[10][20];
     int i=0;
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            d1[j] = ' ';
+            d2[i][j] = ' ';
+        }
+    }
     for(int j=0; j<10; j++){
         cin>>d2[j];
     }
     cin>>d1;
-    if(f(d2, d1, i)==true){
-        cout<<i+1;
+    if(f(d2, d1, i)==1){
+        cout<<i;
     }else{
         cout<<"non presente"<<endl;
     }
